7ce9e5fed4eb6f4e8145d6ee6995c30f9aa93f9a
vinothini-dhanasekar <vinothinidhanasekar0@gmail.com>
2020-08-18 14:43:02 +0530

    "second commit"

--- gitproject/src/gitproject/Addition.java
+++ gitproject/src/gitproject/Addition.java
@@ -9,6 +9,8 @@ public class Addition {
 		      sum = num1 + num2;
 
 		      System.out.println("Sum of these numbers: "+sum);
+		      System.out.println("Sum of these numbers: "+sum);
+		      System.out.println("Sum of these numbers: "+sum);
 		   }
 		}
 }
